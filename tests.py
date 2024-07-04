e2c10e989c30be39688b00b34582a2b1b6301633
Anastasia Igolkina <an.igolkina13@yandex.ru>
2024-07-04 23:57:54 +0300

    add parametrize tests

--- tests.py
+++ tests.py
@@ -1,15 +1,23 @@
+import pytest
 from main import BooksCollector
 
 
 class TestBooksCollector:
 
-    def test_add_new_book_add_two_books(self):
+    @pytest.mark.parametrize(
+        'name',
+        [
+            'Гордость и предубеждение и зомби',
+            'Что делать, если ваш кот хочет вас убить',
+            'Три богатыря'
+        ]
+    )
+    def test_add_new_book_add_two_books(self, name):
         collector = BooksCollector()
 
-        collector.add_new_book('Гордость и предубеждение и зомби')
-        collector.add_new_book('Что делать, если ваш кот хочет вас убить')
+        collector.add_new_book(name)
 
-        assert len(collector.books_genre) == 2
+        assert len(collector.books_genre) == 1
 
     def test_set_book_genre_add_genre(self):
         collector = BooksCollector()
